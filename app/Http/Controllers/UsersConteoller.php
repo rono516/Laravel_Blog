d5ba1f55da603b69887f62d8da989439f1753e50
rono516 <ronocollins2000@gmail.com>
2021-10-07 02:30:34 +0300

    e-commerce commit

---
+++ app/Http/Controllers/UsersConteoller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Users extends Controller
+{
+    function getData(){
+        return "form data will be here";
+    }
+}
---
+++ resources/views/users.blade.php
@@ -0,0 +1,6 @@
+<h1>User Login</h1>
+<form action="users" method="POST">
+    <input type="text" name="username" placeholder="enter user id"> <br> <br>
+    <input type="password" name="userpassword" placeholder="enter user password"> <br> <br>
+    <button type="submit">Login</button>
+</form>
